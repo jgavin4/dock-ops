3b6fbe58254230d6f58b30767230e3e4ad34cd1a
Jake Gavin <jakemgavin4@gmail.com>
2026-03-03 16:49:49 -0500

    JMG(main): Use undepracted information
    Impact:
    -
    
    Details:
    -

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -29,8 +29,7 @@ export default function RootLayout({
           publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
           signInUrl="/sign-in"
           signUpUrl="/sign-up"
-          afterSignInUrl="/"
-          afterSignUpUrl="/"
+          fallbackRedirectUrl="/"
         >
           <Providers>
             <Header />
